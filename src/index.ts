67d40963c765fd4d400eec52920004ef4de07d0a
shakbr <shanshiashvilishako@gmail.com>
2023-12-06 20:20:46 +0400

    setting up posgres database in docker with all the configurations

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import * as dotenv from 'dotenv';
+dotenv.config();
+
 import compression from 'compression';
 import cookieParser from 'cookie-parser';
 import express, { Application } from 'express';
@@ -6,11 +8,23 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import validateEnv from '@utils/validateEnv';
 import routes from './routes';
+import { DataSource } from 'typeorm';
+import config from './config/ormconfig';
 
-dotenv.config();
 validateEnv();
 
-const ExpressConfig = (): Application => {
+const dataSource = new DataSource(config);
+
+const ExpressConfig = async (): Promise<Application> => {
+  // Initialize the TypeORM connection
+  await dataSource
+    .initialize()
+    .then(() => console.log('Database connected successfully'))
+    .catch((error) => {
+      console.error('Error during Data Source initialization', error);
+      process.exit(1);
+    });
+
   const app = express();
   app.use(compression());
   app.use(express.urlencoded({ extended: true }));
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,15 @@
 import ExpressConfig from '.';
 
-const app = ExpressConfig();
-
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log('Server Running on Port' + PORT));
+const startServer = async () => {
+  try {
+    const app = await ExpressConfig();
+    app.listen(PORT, () => console.log(`Server Running on Port ${PORT}`));
+  } catch (error) {
+    console.error('Error starting the server', error);
+    process.exit(1);
+  }
+};
+
+startServer();
